492c21f03b7907fd46a56dc6fc9f5c9c766b77ca
NozirTukhtamurodov <nozir9407@gmail.com>
2023-11-08 20:04:38 +0500

    first draft

---
+++ ETL/main.py
@@ -0,0 +1,101 @@
+from decorators import create_session, coroutine, backoff
+from time import sleep
+import logging as logger
+from repository import FilmWorkRepository, GenreRepository, PersonRepository, BaseRepository
+from elastic.indexes import MoviesIndex, GenresIndex, PersonsIndex, Index
+from typing import Generator
+from datetime import datetime
+from states.state import State
+from states.redis_storage import RedisStorage
+
+
+STATE_KEY = 'last_{index}_updated'
+
+def repository_index():
+    return [
+        {
+            'repository': FilmWorkRepository,
+            'index': MoviesIndex,
+        },
+        {
+            'repository': GenreRepository,
+            'index': GenresIndex,
+        },
+        {
+            'repository': PersonRepository,
+            'index': PersonsIndex,
+        },
+    ]
+
+
+@coroutine
+@create_session
+@backoff(start_sleep_time=0.1, factor=2, border_sleep_time=100)
+def fetch_changed_objects(next_node: Generator, session) -> Generator[None, datetime, None]:
+    ''' 
+    checks if there any updated movies
+    '''
+    while data := (yield):
+        last_updated = data['last_updated']
+        repository: BaseRepository = data['repository']
+        with session:
+            records_per_page = 100
+            offset = 0
+            while True:
+                query = repository.last_updateds(limit=records_per_page, offset=offset, updated_at=last_updated)
+                result = session.execute(query)
+                objects = result.mappings().all()
+                print(objects, '#####################')
+                ## break the loop if there is no data
+                if not result or not objects:
+                    break
+                data['objects'] = objects
+                data['updated_at'] = str(objects[-1]['updated_at'])
+                offset += records_per_page
+                next_node.send(data)
+
+
+@coroutine
+@backoff(start_sleep_time=0.1, factor=2, border_sleep_time=100)
+def save_data(next_node: Generator) -> Generator[None, list, None]:
+    '''
+    saves the updated movies to ES
+    
+    '''
+    while data := (yield):
+        objects = data['objects']
+        index: Index = data['index']
+        index.put_data(data=objects)
+        logger.info(f'Received for saving {len(objects)} movies')
+        next_node.send(data)
+
+
+@coroutine
+@backoff(start_sleep_time=0.1, factor=2, border_sleep_time=100)
+def save_state(state: State) -> Generator[None, list, None]:
+    '''
+    saves the updated_at field into state
+    '''
+    while data := (yield):
+        updated_at = data['updated_at']
+        index = data['index']
+        logger.info(f'Recived for saving last updated at: {updated_at} to state')
+        state.set_state(STATE_KEY.format(index=index), updated_at)
+
+
+@backoff(start_sleep_time=0.1, factor=2, border_sleep_time=100)
+def main():
+    state = State(RedisStorage())
+    repo_ind = repository_index()
+    state_saver_coro: Generator = save_state(state)
+    movies_saver_coro: Generator = save_data(next_node=state_saver_coro)
+    fetcher_coro: Generator = fetch_changed_objects(next_node=movies_saver_coro)
+    while True:
+        for data in repo_ind:
+            data['last_updated'] = state.get_state(STATE_KEY.format(index=data['index'])) or datetime.min
+            fetcher_coro.send(data)
+            print(data)
+        sleep(15)
+
+if __name__ == "__main__":
+    main()
